b20170dd924754424acb1e8bf8e5f5783ccd0bf9
rickdouglas <rickmiranda23@gmail.com>
2022-10-12 21:49:58 -0300

    Estilizando o componente Post

--- react/01-fundamentos-react-js/src/App.jsx
+++ react/01-fundamentos-react-js/src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header"
 import { Sidebar } from "./components/Sidebar"
+import { Post } from "./components/Post";
 import './styles/global.css'
 import styles from './App.module.css';
 
@@ -11,7 +12,8 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <p>Aqui vai ficar os posts</p>
+          <Post />
+          <Post />
         </main>
         
       </div>
---
+++ react/01-fundamentos-react-js/src/components/Post.jsx
@@ -0,0 +1,31 @@
+import styles from '../styles/Post.module.css'
+import profileImg from '../assets/profile.png'
+
+export function Post () {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src={profileImg} />
+                    <div className={styles.authorInfo}>
+                        <strong>Rick Douglas</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title='11 de outubro as 12:30' dateTime='2022-10-12 18:42:30'>Publicado ha 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉 <a href=''> jane.design/doctorcare</a></p>
+                <p>
+                    <a href=''>#novoprojeto </a>
+                    <a href="">#nlw </a>
+                    <a href>#rocketseat</a>
+                </p>
+            </div>
+        </article>
+    )
+} 
